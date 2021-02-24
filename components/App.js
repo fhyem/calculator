08a1e8b085f23039cb72cbbfa4260bb02959daa7
Fahim Ud Din <fhyembercha@gmail.com>
2021-02-24 17:59:51 +0500

    trying to figure out the logic for the operations

--- components/App.js
+++ components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "./app.css";
 
 function App() {
@@ -8,23 +8,31 @@ function App() {
   const [operator, setOperator] = useState("");
 
   const numInput = (e) => {
-    if (result === 0) {
-      setResult(e.target.value);
-    } else {
-      if (operator === "") {
-        setResult(result + e.target.value);
-      } else {
-        setFirstValue(result);
-      }
-      console.log(this.state.result);
-      console.log(this.state.firstValue);
-    }
+    // if (result === 0 || operator !== "") {
+    //   let firstNum = e.target.value;
+    //   return setResult(firstNum);
+    // } else {
+    //   if (operator === "") {
+    //     let firstNum = result + e.target.value;
+    //     return setResult(firstNum);
+    //   } else {
+    //     const valueOne = result;
+    //     setFirstValue(valueOne);
+    //     setResult(0);
+    //     const valueTwo = e.target.value;
+    //     setSecondValue(valueTwo);
+    //     setFirstValue(result);
+    //     setSecondValue(e.target.value);
+    //     setResult(e.target.value);
+    //     console.log(firstValue);
+    //     console.log(e.target.value);
+    //     console.log(secondValue);
+    //   }
+    // }
   };
 
-  const operandInput = (value) => {
-    this.setState({
-      operator: value.target.value,
-    });
+  const operandInput = (operator) => {
+    setOperator(operator.target.value);
   };
 
   const clearResult = () => {
@@ -36,9 +44,29 @@ function App() {
     });
   };
 
+  const calculate = () => {
+    switch (operator) {
+      case "+":
+        setResult(firstValue + secondValue);
+        console.log("Plus clicked");
+        break;
+      case "-":
+        setResult(firstValue - secondValue);
+        break;
+      case "X":
+        setResult(firstValue * secondValue);
+        break;
+      case "/":
+        setResult(firstValue / secondValue);
+        break;
+    }
+  };
+
   const showResult = () => {
-    if (this.state.secondValue !== "") {
-      const result = `${this.state.firstValue}${this.state.operator}${this.state.secondValue}`;
+    if (secondValue !== "") {
+      setResult(calculate());
+    } else {
+      setResult("Put second value");
     }
   };
 
@@ -47,35 +75,35 @@ function App() {
       <h1>Calculator React</h1>
       <div className="container">
         <div className="header">Calculator</div>
-        <input type="text" className="result" value={this.state.result} />
+        <input type="text" className="result" value={result} />
         <div className="second-row">
           <input
             type="button"
             name=""
             value="7"
             className="global seven"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="8"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="9"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="/"
             className="global divide"
-            onClick={(operator) => this.operandInput(operator)}
+            onClick={(operator) => operandInput(operator)}
           />
         </div>
         <div className="third-row">
@@ -84,28 +112,28 @@ function App() {
             name=""
             value="4"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="5"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="6"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="X"
             className="global multiply"
-            onClick={(operator) => this.operandInput(operator)}
+            onClick={(operator) => operandInput(operator)}
           />
         </div>
         <div className="fourth-row">
@@ -114,28 +142,28 @@ function App() {
             name=""
             value="1"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="2"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="3"
             className="global"
-            onClick={(e) => this.numInput(e)}
+            onClick={(e) => numInput(e)}
           />
           <input
             type="button"
             name=""
             value="-"
             className="global minus"
-            onClick={(operator) => this.operandInput(operator)}
+            onClick={(operator) => operandInput(operator)}
           />
         </div>
         <div className="conflict">
@@ -145,28 +173,28 @@ function App() {
               name=""
               value="0"
               className="global big zero"
-              onClick={(e) => this.numInput(e)}
+              onClick={(e) => numInput(e)}
             />
             <input
               type="button"
               name=""
               value="."
               className="global small dot"
-              onClick={(e) => this.numInput(e)}
+              onClick={(e) => numInput(e)}
             />
             <input
               type="button"
               name=""
               value="Del"
               className="red small white-text top-margin delete"
-              onClick={() => this.clearResult()}
+              onClick={() => clearResult()}
             />
             <input
               type="button"
               name=""
               value="="
               className="green white-text big top-margin equal"
-              onClick={() => this.showResult()}
+              onClick={() => showResult()}
             />
           </div>
           <div className="right">
@@ -175,7 +203,7 @@ function App() {
               name=""
               value="+"
               className="global grey plus"
-              onClick={(operator) => this.operandInput(operator)}
+              onClick={(operator) => operandInput(operator)}
             />
           </div>
         </div>
